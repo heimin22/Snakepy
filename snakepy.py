6383d6e21bb40c34d5186a43c51747c4a1e18671
heimin22 <fyketonel@gmail.com>
2025-04-25 02:50:04 +0800

    methods para sa width and height ng screen, tapos reset ng game, yung spawn din ng pagkain.

--- snakepy.py
+++ snakepy.py
@@ -14,3 +14,30 @@ UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
+
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+class SnakeGame:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.reset()
+
+    def reset(self):
+        self.snake = [(self.width // 2, self.height // 2)]
+        self.direction = RIGHT
+        self.spawn_food()
+        self.score = 0
+        self.game_over = False
+
+    def spawn_food(self):
+        while True:
+            self.food = (
+                random.randint(1, self.width - 2),
+                random.randint(1, self.height - 2),
+            )
+            if self.food not in self.snake:
+                break
