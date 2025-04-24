b0f1da19730799d416183cd3319b0a85d0aa839d
heimin22 <fyketonel@gmail.com>
2025-04-25 02:41:10 +0800

    Added initial commits

---
+++ snakepy.py
@@ -0,0 +1,16 @@
+import os
+import random
+import time
+import msvcrt
+
+# controls are
+# w = up, s = down, a = left, d = right, q = quit
+
+WIDTH = 40
+HEIGHT = 20
+
+# directions
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
