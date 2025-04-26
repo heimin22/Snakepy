7baf1ec77c7cff0a305776817131304a209ab3a3
heimin22 <fyketonel@gmail.com>
2025-04-27 07:31:53 +0800

    currently updating the game, creating a new feature where the user can input the sides they want and the game will generate a random shape as a map

--- snakepy.py
+++ snakepy.py
@@ -91,3 +91,93 @@ def gameLoop():
 
     snake_List = []
     Length_of_snake = 1
+
+    while True:
+        foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+        foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+        if not is_point_in_polygon(foodx, foody, sides, radius):
+            break
+
+    while not game_over:
+        while game_close:
+            dis.fill(blue)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        next_x = x1 + x1_change
+        next_y = y1 + y1_change
+        if not is_point_in_polygon(next_x, next_y, sides, radius):
+            game_close = True
+
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(blue)
+
+        points = []
+        for i in range(sides)
+            angle = 2 * math.pi * i / sides
+            x = dis_width / 2 + radius * math.cos(angle)
+            y = dis_height / 2 + radius * math.sin(angle)
+            points.append((x, y))
+        pygame.draw.polygon(dis, white, points, 2)
+
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            while True:
+                foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+                foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+                if is_point_in_polygon(foodx, foody, sides, radius):
+                    break
+            Length_of_snake += 1
+
+        clock.tick(snake_speed)
+    
+    pygame.quit()
+    quit()
+
+gameLoop()
+
+
+
+
